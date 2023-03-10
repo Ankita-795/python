0a4a5235c44ca74a909b58e75293ae7fc0ceaf19
Ankita-795 <127400151+Ankita-795@users.noreply.github.com>
2023-03-10 11:24:21 +0530

    Update and rename add.c.txt to add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int num1,num2,sum=0;
+  printf("Please,Enter any two integer number = ");
+  scanf("%d %d",&num1,&num2);
+  
+  sum=num1+num2;
+  printf("Addition of two num = %d",sum);
+  getch();
+}
